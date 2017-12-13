b3e5ec37a20b87e55f97139d7ceca882e9cd40b2
vaidy083 <vaidy083@umn.edu>
2017-12-12 21:30:09 -0600

    third commit

--- src/regression.java
+++ src/regression.java
@@ -1,4 +1,3 @@
-
 import java.util.*;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -13,157 +12,162 @@ import packages.vector;
  * and open the template in the editor.
  */
 /**
- *
+ * 
  * @author vivek
  */
 public class regression {
 
-    static String testFile = "project_3/rep1/mnist_test.csv";
-    static String trainFile = "project_3/rep1/mnist_train.csv";
-    static String valFile = "project_3/rep1/mnist_validation.csv";
+	static String testFile = "/home/grad06/vaidy083/workspace/DMAssignment3/project_3/rep3/mnist_test.csv";
+	static String trainFile = "/home/grad06/vaidy083/workspace/DMAssignment3/project_3/rep3/mnist_train.csv";
+	static String valFile = "/home/grad06/vaidy083/workspace/DMAssignment3/project_3/rep3/mnist_validation.csv";
 
-    static double[] lambdas = new double[]{0.01, 0.05, 0.1, 0.5, 1.0, 2.0, 5.0};
-    static double stopping = 0.0001;
+	static double[] lambdas = new double[] { 0.01, 0.05, 0.1, 0.5, 1.0, 2.0,
+			5.0 };
+	static double stopping = 0.0001;
 
-    public static void main(String[] args) {
-        System.out.println("Fetching");
-        double[][] testSet = FileOps.getCSVContentAsMatrix(testFile);
-        double[][] trainSet = FileOps.getCSVContentAsMatrix(trainFile);
-        double[][] valSet = FileOps.getCSVContentAsMatrix(valFile);
+	public static void main(String[] args) {
+		long starting = System.currentTimeMillis();
+		System.out.println("Fetching");
+		double[][] testSet = FileOps.getCSVContentAsMatrix(testFile);
+		double[][] trainSet = FileOps.getCSVContentAsMatrix(trainFile);
+		double[][] valSet = FileOps.getCSVContentAsMatrix(valFile);
 
-        double[][] trainingvals = CSR.dropColumn(trainSet, 0);
-        int[] order = permute(trainingvals[0].length);
+		double[][] trainingvals = CSR.dropColumn(trainSet, 0);
+		int[] order = permute(trainingvals[0].length);
 
-        CSR X = CSR.toCSR(trainingvals);
-        double weights[][][] = new double[lambdas.length][10][trainingvals[0].length];
+		CSR X = CSR.toCSR(trainingvals);
+		double weights[][][] = new double[lambdas.length][10][trainingvals[0].length];
 
-        vector[] xi = getxis(trainingvals);
-        double l2[] = new double[xi.length];
+		vector[] xi = getxis(trainingvals);
+		double l2[] = new double[xi.length];
 
-        for (int i = 0; i < xi.length; i++) {
-            l2[i] = CSR.vectormult(xi[i], xi[i]);
-        }
+		for (int i = 0; i < xi.length; i++) {
+			l2[i] = CSR.vectormult(xi[i], xi[i]);
+		}
 
-        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newCachedThreadPool();
+		for(int i = 0; i< lambdas.length; i++){
+			for(int j = 0; j < 10; j++){
+				weights[i][j] = fillrands(weights[i][j]);	
+			}
+		}
+		ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors
+				.newCachedThreadPool();
 
-        for (int i = 0; i < lambdas.length; i++) {
-            for (int Cl = 0; Cl < 10; Cl++) {
+		for (int i = 0; i < lambdas.length; i++) {
+			for (int Cl = 0; Cl < 10; Cl++) {
 
-                WorkerThread task = new WorkerThread(
-                        X,
-                        weights[i][Cl],
-                        lambdas[i],
-                        Cl,
-                        getYVals(Cl, trainSet),
-                        xi,
-                        l2,
-                        order);
-                executor.execute(task);
-            }
-        }
-        executor.shutdown();
-        while (!executor.isTerminated()) {
-        }
+				WorkerThread task = new WorkerThread(X, weights[i][Cl],
+						lambdas[i], Cl, getYVals(Cl, trainSet), xi, l2, order);
+				executor.execute(task);
+			}
+		}
+		executor.shutdown();
+		while (!executor.isTerminated()) {
+		}
 
-        double bestacc = -1;
-        int bestlambda = -1;
-        for (int i = 0; i < lambdas.length; i++) {
-            double temp = validate(weights[i], valSet);
-            if (temp > bestacc) {
-                bestacc = temp;
-                bestlambda = i;
-            }
-        }
+		double bestacc = -1;
+		int bestlambda = -1;
+		for (int i = 0; i < lambdas.length; i++) {
+			double temp = validate(weights[i], valSet);
+			if (temp > bestacc) {
+				bestacc = temp;
+				bestlambda = i;
+			}
+		}
 
-        System.out.println();
-    }
+		System.out.println("BESTLambda: " + lambdas[bestlambda]);
+		System.out.println("BESTACC: " + bestacc);
+		System.out.println("time: " + (System.currentTimeMillis() - starting)
+				/ 1000);
+	}
 
-    static double validate(double[][] w, double[][] valSet) {
-        double[][] validationarray = CSR.dropColumn(valSet, 0);
-        double score = 0;
-        for (int j = 0; j < validationarray.length; j++) {
-            double[] currentarr = validationarray[j];
-            double argmax = -1;
-            double max = -1;
-            for (int i = 0; i < 10; i++) {
-                if (CSR.vectormult(currentarr, w[i]) > max) {
-                    max = CSR.vectormult(currentarr, w[i]);
-                    argmax = i;
-                }
-            }
-            if (argmax == valSet[j][0]) {
-                score++;
-            }
-        }
-        return score / (double) valSet.length;
-    }
+	static double validate(double[][] w, double[][] valSet) {
+		double[][] validationarray = CSR.dropColumn(valSet, 0);
+		double score = 0;
+		for (int j = 0; j < validationarray.length; j++) {
+			double[] currentarr = validationarray[j];
+			double argmax = -1;
+			double max = -1;
+			for (int i = 0; i < 10; i++) {
+				if (CSR.vectormult(currentarr, w[i]) > max) {
+					max = CSR.vectormult(currentarr, w[i]);
+					argmax = i;
+				}
+			}
+			if (argmax == valSet[j][0]) {
+				score++;
+			}
+		}
+		return score / (double) valSet.length;
+	}
 
-    static double[] norm(double[] a) {
-        for (int i = 0; i < a.length; i++) {
-            a[i] = (a[i] >= 0) ? 1 : -1;
-        }
-        return a;
-    }
+	static double[] norm(double[] a) {
+		for (int i = 0; i < a.length; i++) {
+			a[i] = (a[i] >= 0) ? 1 : -1;
+		}
+		return a;
+	}
 
-    static double geterr(CSR X, double[] w, double[] y) {
-        double[] ans = CSR.matsub(y, norm(CSR.multiply(X, CSR.toVector(w))));
-        double res = 0;
-        for (double x : ans) {
-            res += x * x;
-        }
-        return res;
-    }
+	static double geterr(CSR X, double[] w, double[] y) {
+		double[] ans = CSR.matsub(y, norm(CSR.multiply(X, CSR.toVector(w))));
+		double res = 0;
+		for (double x : ans) {
+			res += x * x;
+		}
+		return res;
+	}
 
-    static int[] permute(int length) {
-        int[] output = new int[length];
-        List<Integer> picker = new ArrayList<>();
-        for (int i = 0; i < length; i++) {
-            picker.add(i);
-        }
-        Random r = new Random();
-        for (int i = 0; i < length; i++) {
-            int x = r.nextInt(picker.size());
-            int y = picker.get(x);
-            picker.remove(x);
-            output[i] = y;
-        }
-        return output;
-    }
+	static int[] permute(int length) {
+		int[] output = new int[length];
+		List<Integer> picker = new ArrayList<>();
+		for (int i = 0; i < length; i++) {
+			picker.add(i);
+		}
+		Random r = new Random();
+		for (int i = 0; i < length; i++) {
+			int x = r.nextInt(picker.size());
+			int y = picker.get(x);
+			picker.remove(x);
+			output[i] = y;
+		}
+		return output;
+	}
 
-    static CSR[] getXminuses(double[][] vals) {
-        CSR[] res = new CSR[vals[0].length];
+	static CSR[] getXminuses(double[][] vals) {
+		CSR[] res = new CSR[vals[0].length];
 
-        for (int i = 0; i < vals[0].length; i++) {
-            res[i] = CSR.toCSR(CSR.dropColumn(vals, i));
-        }
-        return res;
-    }
+		for (int i = 0; i < vals[0].length; i++) {
+			res[i] = CSR.toCSR(CSR.dropColumn(vals, i));
+		}
+		return res;
+	}
 
-    static double[] fillrands(double[] f) {
-        Random rand = new Random();
-        for (int i = 0; i < f.length; i++) {
-            f[i] = rand.nextDouble() * ((rand.nextInt(1) == 0) ? 1 : -1) / 1000.0;
-        }
-        return f;
-    }
+	static double[] fillrands(double[] f) {
+		Random rand = new Random();
+		for (int i = 0; i < f.length; i++) {
+			f[i] = rand.nextDouble() * ((rand.nextInt(1) == 0) ? 1 : -1)
+					/ 1000.0;
+		}
+		return f;
+	}
 
-    static final double[] getYVals(int a, double[][] b) {
-        double[] res = new double[b.length];
-        for (int i = 0; i < b.length; i++) {
-            res[i] = (b[i][0] == a) ? 1 : -1;
-        }
-        return res;
-    }
+	static final double[] getYVals(int a, double[][] b) {
+		double[] res = new double[b.length];
+		for (int i = 0; i < b.length; i++) {
+			res[i] = (b[i][0] == a) ? 1 : -1;
+		}
+		return res;
+	}
 
-    static vector[] getxis(double[][] a) {
-        vector[] output = new vector[a[0].length];
-        for (int j = 0; j < a[0].length; j++) {
-            double[] line = new double[a.length];
-            for (int i = 0; i < a.length; i++) {
-                line[i] = a[i][j];
-            }
-            output[j] = CSR.toVector(line);
-        }
-        return output;
-    }
+	static vector[] getxis(double[][] a) {
+		vector[] output = new vector[a[0].length];
+		for (int j = 0; j < a[0].length; j++) {
+			double[] line = new double[a.length];
+			for (int i = 0; i < a.length; i++) {
+				line[i] = a[i][j];
+			}
+			output[j] = CSR.toVector(line);
+		}
+		return output;
+	}
 }
